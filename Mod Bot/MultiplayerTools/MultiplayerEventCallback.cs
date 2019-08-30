1f98fc62c24064b984f12ba6b8a4554be00c0e9f
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2019-08-30 20:12:14 +0200

    Added all base game multiplayer events to  ModdedMultiplayerEventListener

---
+++ Mod Bot/MultiplayerTools/MultiplayerEventCallback.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bolt;
+
+namespace ModLibrary
+{
+    /// <summary>
+    /// Methods defined in this class can add and remove listeners to <see cref="Event"/> and invoke methods when that <see cref="Event"/> is recieved by our client
+    /// </summary>
+    public static class MultiplayerEventCallback
+    {
+        private static Dictionary<Type, List<object>> eventListeners = new Dictionary<Type, List<object>>();
+
+        private static void AddEventTypeToDictionary(Type eventType)
+        {
+            if (!eventListeners.ContainsKey(eventType))
+            {
+                eventListeners.Add(eventType, new List<object>());
+            }
+        }
+
+        private static bool HasCallbackDefined<T>(Action<T> callback) where T : Event
+        {
+            if (eventListeners[typeof(T)] == null)
+            {
+                return false;
+            }
+
+            return eventListeners[typeof(T)].Contains(callback);
+        }
+
+        /// <summary>
+        /// Add a callback to a method from event of type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="Event"/> to add the callback to</typeparam>
+        /// <param name="callback">The <see cref="Action{T1}"/> to call when the <see cref="Event"/> <typeparamref name="T"/> is recieved</param>
+        /// <param name="allowDuplicate">Determines if multiple callbacks to the same method should be allowed</param>
+        public static void AddEventListener<T>(Action<T> callback, bool allowDuplicate = false) where T : Event
+        {
+            AddEventTypeToDictionary(typeof(T));
+
+            if (!HasCallbackDefined(callback))
+            {
+                eventListeners[typeof(T)].Add(callback);
+            }
+        }
+
+        /// <summary>
+        /// Remove the given callback from the callback list for the given <see cref="Event"/> type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="Event"/> to remove a callback from</typeparam>
+        /// <param name="callback">The <see cref="Action{T}"/> to remove from the callback list</param>
+        public static void RemoveEventListener<T>(Action<T> callback) where T : Event
+        {
+            AddEventTypeToDictionary(typeof(T));
+            
+            eventListeners[typeof(T)].Remove(callback);
+        }
+
+        internal static void OnEventRecieved<T>(T _event) where T : Event
+        {
+            if (!eventListeners.ContainsKey(typeof(T)))
+            {
+                return;
+            }
+
+            List<object> callbacks = eventListeners[typeof(T)];
+            for (int i = 0; i < callbacks.Count; i++)
+            {
+                Action<T> callbackAction = callbacks[i] as Action<T>;
+
+                if (callbackAction == null)
+                {
+                    callbacks.RemoveAt(i);
+                    i--;
+                    continue;
+                }
+
+                callbackAction(_event);
+            }
+        }
+    }
+}
--- Mod Bot/MultiplayerTools/MultiplayerMessageSender.cs
+++ Mod Bot/MultiplayerTools/MultiplayerMessageSender.cs
@@ -8,20 +8,24 @@ using UnityEngine;
 
 namespace ModLibrary
 {
+    /// <summary>
+    /// Used to send generic string messages
+    /// </summary>
     public static class MultiplayerMessageSender
     {
         /// <summary>
-        /// Sends a multiplayer message the owner of a FirstPersonMover
+        /// Sends a multiplayer message the owner of a <see cref="FirstPersonMover"/>
         /// </summary>
-        /// <param name="owner">Player to send message to</param>
+        /// <param name="owner">The player to send message to</param>
         /// <param name="message">The message to send</param>
         public static void SendToClient(FirstPersonMover owner, string message)
         {
             GenericStringForModdingEvent genericStringForModdingEvent = GenericStringForModdingEvent.Create(owner.entity.controller);
             SendEvent(genericStringForModdingEvent, message);
         }
+
         /// <summary>
-        /// Sends a multiplayer message to a bolt connection
+        /// Sends a multiplayer message to a <see cref="BoltConnection"/>
         /// </summary>
         /// <param name="owner">The player to send the message to</param>
         /// <param name="message">The message to send</param>
@@ -31,10 +35,24 @@ namespace ModLibrary
             SendEvent(genericStringForModdingEvent, message);
         }
 
-
+        /// <summary>
+        /// Sends the given <paramref name="message"/> to all clients (including ourselves) connected to the same server we are connected to
+        /// </summary>
+        /// <param name="message">The message to send</param>
         public static void SendToAllClients(string message)
         {
             GenericStringForModdingEvent genericStringForModdingEvent = GenericStringForModdingEvent.Create(GlobalTargets.AllClients);
+            SendEvent(genericStringForModdingEvent, message);
+        }
+
+        /// <summary>
+        /// Sends the given <paramref name="message"/> to the given <see cref="GlobalTargets"/>
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="targets"></param>
+        public static void SendToClients(string message, GlobalTargets targets)
+        {
+            GenericStringForModdingEvent genericStringForModdingEvent = GenericStringForModdingEvent.Create(targets);
             SendEvent(genericStringForModdingEvent, message);
         }
 
@@ -42,11 +60,11 @@ namespace ModLibrary
         {
             if (message.Length > 140)
             {
-                throw new Exception("Attempted to send a message that is too long, all messages must be under 140 characters");
+                throw new Exception("Error sending modded string event: Attempted to send a message that is too long, messages cannot exceed 140 characters in length");
             }
+
             myEvent.EventData = message;
             myEvent.Send();
         }
-        
     }
 }
