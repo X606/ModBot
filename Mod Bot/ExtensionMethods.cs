3ee1a4a4a8f7abd9c59fcbf8a354fcbd2415e3ae
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2019-08-31 13:27:15 +0200

    Added extension methods to IEnumerable<T> for getting all field and property values and calling methods in each instance of the IEnumerable<T>

--- Mod Bot/ExtensionMethods.cs
+++ Mod Bot/ExtensionMethods.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using InternalModBot;
 
 namespace ModLibrary
@@ -14,7 +17,7 @@ namespace ModLibrary
         /// <typeparam name="T">The type of the object at the index</typeparam>
         /// <param name="moddedObject"></param>
         /// <param name="index">The index of the object you want to get</param>
-        /// <returns>The object you asked for</returns>
+        /// <returns>The object at the specified index, casted to type <typeparamref name="T"/></returns>
         public static T GetObject<T>(this ModdedObject moddedObject, int index) where T : UnityEngine.Object
         {
             if (index < 0 || index >= moddedObject.objects.Count)
@@ -29,6 +32,22 @@ namespace ModLibrary
         }
 
         /// <summary>
+        /// Returns true of the mod is enbaled, false if its disabled
+        /// </summary>
+        /// <param name="mod"></param>
+        /// <returns></returns>
+        public static bool IsModEnabled(this Mod mod)
+        {
+            bool? isModDeactivated = ModsManager.Instance.IsModDeactivated(mod);
+
+            if (!isModDeactivated.HasValue)
+                throw new Exception("Mod \"" + mod.GetModName() + "\" with unique id \"" + mod.GetUniqueID() + "\" could not found in modsmanager's list of mods!");
+
+            return !isModDeactivated.Value;
+        }
+
+        #region Mod Settings Extensions
+        /// <summary>
         /// Sets the icon of the upgrade to a image from a url, this needs a internet connection (NOTE: this has a cache so if you want to change picture you might want to remove the cache in the mods directory)
         /// </summary>
         /// <param name="upgradeDescription"></param>
@@ -37,6 +56,7 @@ namespace ModLibrary
         {
             UpgradeIconDownloader.Instance.AddUpgradeIcon(upgradeDescription, url);
         }
+
         /// <summary>
         /// Gets the settings saved in the loaded settings
         /// </summary>
@@ -49,6 +69,7 @@ namespace ModLibrary
             string result = OptionsSaver.LoadString(mod, name);
             return result;
         }
+
         /// <summary>
         /// Gets the settings saved in the loaded settings
         /// </summary>
@@ -61,6 +82,7 @@ namespace ModLibrary
             bool? result = OptionsSaver.LoadBool(mod, name);
             return result;
         }
+
         /// <summary>
         /// Gets the settings saved in the loaded settings
         /// </summary>
@@ -73,6 +95,7 @@ namespace ModLibrary
             float? result = OptionsSaver.LoadFloat(mod, name);
             return result;
         }
+
         /// <summary>
         /// Gets the settings saved in the loaded settings
         /// </summary>
@@ -85,21 +108,243 @@ namespace ModLibrary
             int? result = OptionsSaver.LoadInt(mod, name);
             return result;
         }
+        #endregion
+
+        #region IEnumerable<T> Extensions
         /// <summary>
-        /// Returns true of the mod is enbaled, false if its disabled
+        /// Gets all fields of the given name in the given <see cref="IEnumerable{T}"/>
         /// </summary>
-        /// <param name="mod"></param>
-        /// <returns></returns>
-        public static bool IsModEnabled(this Mod mod)
+        /// <typeparam name="CollectionType">The type if the original <see cref="IEnumerable{T}"/></typeparam>
+        /// <typeparam name="FieldType">The type to cast all the field values to</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to get the fields from</param>
+        /// <param name="fieldName">The name of the field to get from the <typeparamref name="CollectionType"/>, case-sensitive by default</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the fields</param>
+        /// <returns>A new <see cref="IEnumerable{T}"/> that contains all the found fields with the given name casted to the given <typeparamref name="FieldType"/></returns>
+        public static IEnumerable<FieldType> GetFields<CollectionType, FieldType>(this IEnumerable<CollectionType> collection, string fieldName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
         {
-            bool? isModDeactivated = ModsManager.Instance.IsModDeactivated(mod);
+            FieldInfo field = typeof(CollectionType).GetField(fieldName, bindingFlags);
 
-            if (!isModDeactivated.HasValue)
-                throw new Exception("Mod \"" + mod.GetModName() + "\" with unique id \"" + mod.GetUniqueID() + "\" not found in modsmanager list of mods!");
+            if (Equals(field, null))
+            {
+                throw new MissingFieldException("Field \"" + typeof(CollectionType).Name + "." + fieldName + "\" could not be found!");
+            }
 
-            return !isModDeactivated.Value;
+            FieldType[] fields = new FieldType[collection.Count()];
 
+            int currentIndex = 0;
+            foreach (CollectionType item in collection)
+            {
+                fields[currentIndex] = (FieldType)field.GetValue(item);
+                currentIndex++;
+            }
+
+            return fields;
         }
+
+        /// <summary>
+        /// Gets all properties of the given name in the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type if the original <see cref="IEnumerable{T}"/></typeparam>
+        /// <typeparam name="PropertyType">The type to cast all the property values to</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to get the properties from</param>
+        /// <param name="propertyName">The name of the property to get from the <typeparamref name="CollectionType"/>, case-sensitive by default</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the fields</param>
+        /// <returns>A new <see cref="IEnumerable{T}"/> that contains all the found properties with the given name casted to the given <typeparamref name="PropertyType"/></returns>
+        public static IEnumerable<PropertyType> GetPropertyValues<CollectionType, PropertyType>(this IEnumerable<CollectionType> collection, string propertyName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            PropertyInfo property = typeof(CollectionType).GetProperty(propertyName, bindingFlags);
+
+            if (Equals(property, null))
+            {
+                throw new MissingMemberException("Property \"" + typeof(CollectionType).Name + "." + propertyName + "\" could not be found!");
+            }
+
+            PropertyType[] properties = new PropertyType[collection.Count()];
+
+            int currentIndex = 0;
+            foreach (CollectionType item in collection)
+            {
+                properties[currentIndex] = (PropertyType)property.GetValue(item);
+                currentIndex++;
+            }
+
+            return properties;
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        public static void CallMethods<CollectionType>(this IEnumerable<CollectionType> collection, string methodName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            CallMethods(collection, methodName, arguments: null, bindingFlags: bindingFlags);
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="arguments">The arguments to pass to the method</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        public static void CallMethods<CollectionType>(this IEnumerable<CollectionType> collection, string methodName, object[] arguments, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            Type[] argumentTypes;
+            if (arguments != null)
+            {
+                argumentTypes = new Type[arguments.Length];
+                argumentTypes = Type.GetTypeArray(arguments);
+            }
+            else
+            {
+                argumentTypes = new Type[0];
+            }
+
+            MethodInfo methodInfo = typeof(CollectionType).GetMethod(methodName, bindingFlags, null, argumentTypes, null);
+
+            if (Equals(methodInfo, null))
+            {
+                string typeNames = string.Join(", ", argumentTypes.GetPropertyValues<Type, string>("Name"));
+
+                throw new MissingMethodException("Method \"" + typeof(CollectionType).Name + "." + methodName + "(" + typeNames + ")\" could not be found!");
+            }
+
+            foreach (CollectionType item in collection)
+            {
+                methodInfo.Invoke(item, arguments);
+            }
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="argumentFunction">The <see cref="Func{T, TResult}"/> used to get the argument values for each instance if the <see cref="IEnumerable{T}"/></param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        public static void CallMethods<CollectionType>(this IEnumerable<CollectionType> collection, string methodName, Func<CollectionType, object[]> argumentFunction, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            foreach (CollectionType item in collection)
+            {
+                object[] arguments = argumentFunction(item);
+                Type[] argumentTypes = Type.GetTypeArray(arguments);
+
+                MethodInfo methodInfo = typeof(CollectionType).GetMethod(methodName, bindingFlags, null, argumentTypes, null);
+
+                if (Equals(methodInfo, null))
+                {
+                    string typeNames = string.Join(", ", argumentTypes.GetPropertyValues<Type, string>("Name"));
+
+                    throw new MissingMethodException("Method \"" + typeof(CollectionType).Name + "." + methodName + "(" + typeNames + ")\" could not be found!");
+                }
+
+                methodInfo.Invoke(item, arguments);
+            }
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <typeparam name="ReturnType">The return type of the method called</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        /// <returns>The return values of all the methods called</returns>
+        public static IEnumerable<KeyValuePair<CollectionType, ReturnType>> CallMethods<CollectionType, ReturnType>(this IEnumerable<CollectionType> collection, string methodName, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            return CallMethods<CollectionType, ReturnType>(collection, methodName, arguments: null, bindingFlags: bindingFlags);
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <typeparam name="ReturnType">The return type of the method called</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="arguments">The arguments to pass to all methods called</param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        /// <returns>The return values of all the methods called</returns>
+        public static IEnumerable<KeyValuePair<CollectionType, ReturnType>> CallMethods<CollectionType, ReturnType>(this IEnumerable<CollectionType> collection, string methodName, object[] arguments, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            Type[] argumentTypes;
+            if (arguments != null)
+            {
+                argumentTypes = new Type[arguments.Length];
+                argumentTypes = Type.GetTypeArray(arguments);
+            }
+            else
+            {
+                argumentTypes = new Type[0];
+            }
+
+            MethodInfo methodInfo = typeof(CollectionType).GetMethod(methodName, bindingFlags, null, argumentTypes, null);
+
+            if (Equals(methodInfo, null))
+            {
+                string typeNames = string.Join(", ", argumentTypes.GetPropertyValues<Type, string>("Name"));
+
+                throw new MissingMethodException("Method \"" + typeof(CollectionType).Name + "." + methodName + "(" + typeNames + ")\" could not be found!");
+            }
+
+            KeyValuePair<CollectionType, ReturnType>[] returnValues = new KeyValuePair<CollectionType, ReturnType>[collection.Count()];
+
+            int currentIndex = 0;
+            foreach (CollectionType item in collection)
+            {
+                object returnValue = methodInfo.Invoke(item, arguments);
+                returnValues[currentIndex] = new KeyValuePair<CollectionType, ReturnType>(item, (ReturnType)returnValue);
+
+                currentIndex++;
+            }
+
+            return returnValues;
+        }
+
+        /// <summary>
+        /// Calls a method in all instances of the given <see cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <typeparam name="CollectionType">The type of the <see cref="IEnumerable{T}"/> to call the methods in</typeparam>
+        /// <typeparam name="ReturnType">The return type of the method called</typeparam>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to iterate through</param>
+        /// <param name="methodName">The name of the method to call, case-sesitive by default</param>
+        /// <param name="argumentFunction">The <see cref="Func{T, TResult}"/> used to get the argument values for each instance if the <see cref="IEnumerable{T}"/></param>
+        /// <param name="bindingFlags">The <see cref="BindingFlags"/> used to find the methods</param>
+        /// <returns>The return values of all the methods called</returns>
+        public static IEnumerable<KeyValuePair<CollectionType, ReturnType>> CallMethods<CollectionType, ReturnType>(this IEnumerable<CollectionType> collection, string methodName, Func<CollectionType, object[]> argumentFunction, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+        {
+            KeyValuePair<CollectionType, ReturnType>[] returnValues = new KeyValuePair<CollectionType, ReturnType>[collection.Count()];
+
+            int currentIndex = 0;
+            foreach (CollectionType item in collection)
+            {
+                object[] arguments = argumentFunction(item);
+                Type[] argumentTypes = Type.GetTypeArray(arguments);
+
+                MethodInfo methodInfo = typeof(CollectionType).GetMethod(methodName, bindingFlags, null, argumentTypes, null);
+
+                if (Equals(methodInfo, null))
+                {
+                    string typeNames = string.Join(", ", argumentTypes.GetPropertyValues<Type, string>("Name"));
+
+                    throw new MissingMethodException("Method \"" + typeof(CollectionType).Name + "." + methodName + "(" + typeNames + ")\" could not be found!");
+                }
+
+                object returnValue = methodInfo.Invoke(item, arguments);
+                returnValues[currentIndex] = new KeyValuePair<CollectionType, ReturnType>(item, (ReturnType)returnValue);
+
+                currentIndex++;
+            }
+
+            return returnValues;
+        }
+        #endregion
 
     }
 }
