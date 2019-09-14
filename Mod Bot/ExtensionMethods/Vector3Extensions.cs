f726fda035c41cd124ffdf82d3bbf09a0acf3ef1
Goorakh <petter.ohlen@gmail.com>
2019-09-14 23:57:17 +0200

    Fixed some extension methods being in another namespace

--- Mod Bot/ExtensionMethods/Vector3Extensions.cs
+++ Mod Bot/ExtensionMethods/Vector3Extensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace ModLibrary.ExtensionMethods
+namespace ModLibrary
 {
     /// <summary>
     /// Defines extension methods for <see cref="Vector3"/>
