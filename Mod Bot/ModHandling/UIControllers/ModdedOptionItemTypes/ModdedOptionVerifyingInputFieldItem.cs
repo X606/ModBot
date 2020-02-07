43db8e7e7a4b243610d1a8d99da9e1de1a60aa39
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2020-02-08 00:14:08 +0100

    Added a verifying input field option

---
+++ Mod Bot/ModHandling/UIControllers/ModdedOptionItemTypes/ModdedOptionVerifyingInputFieldItem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModLibrary;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InternalModBot
+{
+    /// <summary>
+    /// Used by Mod-Bot to represent an InputField that verifies input in a modded options page
+    /// </summary>
+    public class ModdedOptionVerifyingInputFieldItem : ModdedOptionInputFieldItem
+    {
+        /// <summary>
+        /// Used to verify the input of the input field, if this returns <see langword="false"/>, the value will revert back to the previous value, and apply it normally otherwise.
+        /// </summary>
+        public Predicate<string> Verify;
+
+        string _oldValue = string.Empty;
+
+        /// <summary>
+        /// Places the page item in the page
+        /// </summary>
+        /// <param name="holder"></param>
+        /// <param name="owner"></param>
+        public override void CreatePageItem(GameObject holder, Mod owner)
+        {
+            GameObject InputFieldPrefab = AssetLoader.GetObjectFromFile("modswindow", "InputField", "Clone Drone in the Danger Zone_Data/");
+            GameObject spawnedPrefab = GameObject.Instantiate(InputFieldPrefab);
+            spawnedPrefab.transform.parent = holder.transform;
+            ModdedObject spawnedModdedObject = spawnedPrefab.GetComponent<ModdedObject>();
+            spawnedModdedObject.GetObject<Text>(0).text = DisplayName;
+            InputField inputField = spawnedModdedObject.GetObject<InputField>(1);
+            inputField.text = DefaultValue;
+
+            string loadedString = OptionsSaver.LoadString(owner, SaveID);
+            if (loadedString != null)
+            {
+                inputField.text = loadedString;
+
+                _oldValue = inputField.text;
+
+                if (OnChange != null)
+                    OnChange(inputField.text);
+            }
+
+            inputField.onEndEdit.AddListener(delegate (string value)
+            {
+                if (!Verify(value))
+                {
+                    inputField.text = _oldValue;
+                    return;
+                }
+
+                _oldValue = value;
+
+                OptionsSaver.SaveString(owner, SaveID, value);
+
+                if(OnChange != null)
+                    OnChange(value);
+            });
+
+            applyCustomRect(spawnedPrefab);
+
+            if (OnCreate != null)
+                OnCreate(inputField);
+        }
+
+    }
+}
