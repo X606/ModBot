2855bbc50add6f82207997865ac3a0726279fbd9
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2021-04-13 12:29:52 +0200

    Fixed a startup crash brought on by a game patch

--- Mod Bot/Properties/Resources.Designer.cs
+++ Mod Bot/Properties/Resources.Designer.cs
@@ -241,7 +241,7 @@ namespace ModLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.5.3.
+        ///   Looks up a localized string similar to 1.5.3.1.
         /// </summary>
         internal static string ModBotVersion {
             get {
