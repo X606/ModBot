7c2a7f9ccd77f0c669a7281769587a6f592b0318
Petter Öhlén <petter.ohlen@gmail.com>
2021-10-24 11:33:55 +0200

    Bump version

--- Mod Bot/Properties/AssemblyInfo.cs
+++ Mod Bot/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mod Bot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A mod loader for Clone Drone in the Danger Zone")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mod Bot")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
--- Mod Bot/Properties/Resources.Designer.cs
+++ Mod Bot/Properties/Resources.Designer.cs
@@ -241,7 +241,7 @@ namespace ModLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.0.2.2.
+        ///   Looks up a localized string similar to 2.0.3.0.
         /// </summary>
         internal static string ModBotVersion {
             get {
