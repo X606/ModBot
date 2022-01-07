8c9dcb2bf6f2fc3b7ac845970177f30782cd09db
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-01-07 16:47:50 +0100

    Bump version

--- Mod Bot/Properties/Resources.Designer.cs
+++ Mod Bot/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ModLibrary.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -241,7 +241,7 @@ namespace ModLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.0.3.0.
+        ///   Looks up a localized string similar to 2.0.3.1.
         /// </summary>
         internal static string ModBotVersion {
             get {
