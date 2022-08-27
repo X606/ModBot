d4d985040925dd59d76fc4db9d2b30a526f0f8bb
Petter Öhlén <petter.ohlen@gmail.com>
2022-08-27 16:08:08 +0200

    Bump version

--- Mod Bot/Properties/Resources.Designer.cs
+++ Mod Bot/Properties/Resources.Designer.cs
@@ -241,7 +241,7 @@ namespace ModLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.0.3.2.
+        ///   Looks up a localized string similar to 2.0.3.3.
         /// </summary>
         internal static string ModBotVersion {
             get {
