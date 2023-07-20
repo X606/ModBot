ab68687836f05ba871caaafca5a6a64af396bb4c
Nikioto4242 <127011626+Nikioto4242@users.noreply.github.com>
2023-07-20 21:04:53 +0500

    added SplitInParts string extension

---
+++ Mod Bot/ModUtilities/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModLibrary
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<String> SplitInParts(this String s, Int32 partLength)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+            if (partLength <= 0)
+                throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+
+            for (var i = 0; i < s.Length; i += partLength)
+                yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+        }
+
+    }
+}
