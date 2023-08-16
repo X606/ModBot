4394aee2b8548763ba0f6d7e58962c80b3c858d6
Nikioto4242 <127011626+Nikioto4242@users.noreply.github.com>
2023-08-16 18:12:31 +0500

    fixed selecting things through mod-bot UI in the level editor

---
+++ Mod Bot/Internal/Patches/LevelEditorUI_Patch.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using ModLibrary;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace InternalModBot
+{
+    [HarmonyPatch(typeof(LevelEditorUI))]
+    static class LevelEditorUI_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("IsAnyDialogueOpen")]
+        static bool IsAnyDialogueOpen_Prefix(ref bool __result)
+        {
+            if (areAnyModdedDialogsOpen())
+            {
+                __result = true;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch("AreAnyDialogsOpen")]
+        static bool AreAnyDialogsOpen_Prefix(ref bool __result)
+        {
+            if (areAnyModdedDialogsOpen())
+            {
+                __result = true;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool areAnyModdedDialogsOpen()
+        {
+            var ui = ModBotUIRoot._instance;
+            return ui.Generic2ButtonDialogeUI.UIRoot.activeSelf ||
+                ui.ModBotSignInUI.WindowObject.activeSelf ||
+                ui.ModCreationWindow.TheGameObject.activeSelf ||
+                ui.ModDownloadPage.WindowObject.activeSelf ||
+                ui.ModOptionsWindow.WindowObject.activeSelf ||
+                ui.ModsWindow.WindowObject.activeSelf;
+        }
+    }
+}
