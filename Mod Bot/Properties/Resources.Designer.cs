745436984d0a74c9ab7cfad106282ac1d33e053b
A TVCat <ai.grishanoff@yandex.ru>
2026-04-25 00:51:51 +0300

    update unity project to 6.3

--- Mod Bot/Properties/Resources.Designer.cs
+++ Mod Bot/Properties/Resources.Designer.cs
@@ -241,7 +241,7 @@ namespace ModLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.0.5.0.
+        ///   Looks up a localized string similar to 2.0.6.0.
         /// </summary>
         internal static string ModBotVersion {
             get {
