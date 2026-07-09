5d5a1e7a74dc5a50dc1ac94707e49435d2c9e217
A TVCat <ai.grishanoff@yandex.ru>
2026-07-09 20:56:29 +0300

    made update checker rely on github instead of the website

--- Mod Bot/Internal/UpdateChecker.cs
+++ Mod Bot/Internal/UpdateChecker.cs
@@ -1,4 +1,5 @@
 ﻿using ModLibrary;
+using Newtonsoft.Json;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -11,6 +12,8 @@ namespace InternalModBot
     /// </summary>
     internal class UpdateChecker : MonoBehaviour
     {
+        public const string BETA_POSTFIX = "-beta";
+
         private static bool s_hasChecked;
 
         private void Start()
@@ -22,7 +25,7 @@ namespace InternalModBot
         private IEnumerator checkVersion()
         {
             string localVersionString = ModLibrary.Properties.Resources.ModBotVersion;
-            if (localVersionString.ToLower().Contains("beta"))
+            if (localVersionString.EndsWith(BETA_POSTFIX))
                 yield break;
 
             if (!Version.TryParse(localVersionString, out Version localVersion))
@@ -31,41 +34,70 @@ namespace InternalModBot
                 yield break;
             }
 
-            using (UnityWebRequest modBotVersionRequest = UnityWebRequest.Get("https://modbot.org/api?operation=getCurrentModBotVersion"))
+            using (UnityWebRequest modBotGitHubVersionRequest = UnityWebRequest.Get("https://api.github.com/repos/X606/ModBot/tags"))
             {
-                modBotVersionRequest.timeout = 10;
-                yield return modBotVersionRequest.SendWebRequest();
+                modBotGitHubVersionRequest.timeout = 10;
+                yield return modBotGitHubVersionRequest.SendWebRequest();
                 s_hasChecked = true;
 
-                if (modBotVersionRequest.result != UnityWebRequest.Result.Success)
+                if (modBotGitHubVersionRequest.result != UnityWebRequest.Result.Success)
                     yield break;
 
-                string remoteVersionString = modBotVersionRequest.downloadHandler.text.Trim().Replace("\"", string.Empty);
-                if (!Version.TryParse(remoteVersionString, out Version remoteVersion))
+                TagData[] tags;
+                try
                 {
-                    debug.Log("Could not parse the REMOTE version of Mod-Bot", Color.yellow);
+                    tags = JsonConvert.DeserializeObject<TagData[]>(modBotGitHubVersionRequest.downloadHandler.text);
+                }
+                catch (Exception exc)
+                {
+                    debug.Log("Could not deserialize tags", Color.yellow);
                     yield break;
                 }
 
-                if (localVersion >= remoteVersion)
+                for (int i = 0; i < tags.Length; i++)
                 {
-                    string modBotUpToDateMessage = ModBotLocalizationManager.FormatLocalizedStringFromID("modbotuptodate", localVersionString);
-                    debug.Log(modBotUpToDateMessage, Color.green);
-                    yield break;
+                    TagData tag = tags[i];
+                    if (tag.IsBeta()) continue;
+
+                    Version remoteVersion = tag.GetVersion();
+                    if (remoteVersion > localVersion)
+                    {
+                        string message = ModBotLocalizationManager.FormatLocalizedStringFromID("newversion_message", remoteVersion.ToString(), localVersionString);
+                        string dismissButtonText = LocalizationManager.Instance.GetTranslatedString("newversion_dismiss");
+                        string installButtonText = LocalizationManager.Instance.GetTranslatedString("newversion_install");
+                        Generic2ButtonDialogue generic = new Generic2ButtonDialogue(message, dismissButtonText, null, installButtonText, onInstallButtonClicked);
+                        generic.SetColorOfFirstButton(Color.red);
+                        generic.SetColorOfSecondButton(Color.green);
+                        yield break;
+                    }
                 }
 
-                string message = ModBotLocalizationManager.FormatLocalizedStringFromID("newversion_message", remoteVersionString, localVersionString);
-                string dismissButtonText = LocalizationManager.Instance.GetTranslatedString("newversion_dismiss");
-                string installButtonText = LocalizationManager.Instance.GetTranslatedString("newversion_install");
-                Generic2ButtonDialogue generic = new Generic2ButtonDialogue(message, dismissButtonText, null, installButtonText, onInstallButtonClicked);
-                generic.SetColorOfFirstButton(Color.red);
-                generic.SetColorOfSecondButton(Color.green);
+                string modBotUpToDateMessage = ModBotLocalizationManager.FormatLocalizedStringFromID("modbotuptodate", localVersionString);
+                debug.Log(modBotUpToDateMessage, Color.green);
             }
         }
 
         private void onInstallButtonClicked()
         {
             Application.OpenURL("https://modbot.org/");
+        }
+
+        private struct TagData
+        {
+            public string name;
+
+            public bool IsBeta() => name.EndsWith(BETA_POSTFIX);
+
+            public Version GetVersion()
+            {
+                string versionString = name.Trim().Trim('v');
+                if (versionString.EndsWith(BETA_POSTFIX))
+                {
+                    versionString = versionString.Substring(0, versionString.Length - BETA_POSTFIX.Length);
+                }
+
+                return new Version(versionString);
+            }
         }
     }
 }
